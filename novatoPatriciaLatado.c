5b12e910c84e7d87afad06f1b0411a9bf3772478
patylatado <patylatado@yahoo.com.br>
2025-04-17 15:25:34 -0300

    Update novatoPatriciaLatado.c
    Atualização do código para o nível Aventureiro

--- novatoPatriciaLatado.c
+++ novatoPatriciaLatado.c
@@ -5,7 +5,7 @@ int main()
     char codigo1[4], cidade1[15], estado1[2], codigo2[4], cidade2[15], estado2[2];
     float area1, PIB1, area2, PIB2, DensPop1, DensPop2, PIBpc1, PIBpc2;
     int populacao1, atracoes1, populacao2, atracoes2;
-    printf("CADASTRO DA CARTA 1\n");
+    printf("CADASTRO DA CARTA 1\n");  //Entrada de dados para a carta 1
     printf("Digite o código da Carta 1\n");
     scanf("%s", codigo1);
     printf("Digite a letra referente ao estado\n");
@@ -20,7 +20,7 @@ int main()
     scanf("%f", &PIB1);
     printf("Digite o número de pontos turísticos\n");
     scanf("%d", &atracoes1);
-    printf("CADASTRO DA CARTA 2\n");
+    printf("CADASTRO DA CARTA 2\n"); //entrada de dados para a carta 2
     printf("Digite o código da Carta 2\n");
     scanf("%s", codigo2);
     printf("Digite a letra referente ao estado\n");
@@ -35,17 +35,17 @@ int main()
     scanf("%f", &PIB2);
     printf("Digite o número de pontos turísticos\n");
     scanf("%d", &atracoes2);
-    DensPop1 = (float)populacao1/area1;
-    DensPop2 = (float) populacao2/area2;
-    PIBpc1 = (float) PIB1/populacao1;
-    PIBpc2 = (float) PIB2/populacao2;
-    printf("\nDADOS DA CARTA 1\n");
+    DensPop1 = (float)populacao1/area1; //calcula a densidade populacional da cidade 1
+    DensPop2 = (float) populacao2/area2; //calcula a densidade populacional da cidade 2
+    PIBpc1 = (float) PIB1/populacao1; //calcula o PIB per capta da cidade 1
+    PIBpc2 = (float) PIB2/populacao2; //calcula o PIB per capta da cidade 2
+    printf("\nDADOS DA CARTA 1\n"); //exibe os dados da carta 1
     printf(" Código: %s\n", codigo1);
     printf(" Estado: %s\n", estado1);
     printf(" Cidade: %s\n", cidade1);
-    printf(" Área: %f km2\n", area1);
+    printf(" Área: %.2f km2\n", area1); //exibe a área da cidade com 2 casas decimais
     printf(" População: %d habitantes\n", populacao1);
-    printf(" PIB: R$ %f\n", PIB1);
+    printf(" PIB: R$ %.1f\n", PIB1); //exibe o PIB da cidade com 1 casa decimal
     printf(" Densidade Populacional: %.2f\n", DensPop1);
     printf(" PIB per capta: %.2f\n", PIBpc1);
     printf(" Número de pontos turísticos: %d\n", atracoes1);
@@ -53,9 +53,9 @@ int main()
     printf(" Código: %s\n", codigo2);
     printf(" Estado: %s\n", estado2);
     printf(" Cidade: %s\n", cidade2);
-    printf(" Área: %f km2\n", area2);
+    printf(" Área: %.2f km2\n", area2);
     printf(" População: %d habitantes\n", populacao2);
-    printf(" PIB: R$ %f\n", PIB2);
+    printf(" PIB: R$ %.1f\n", PIB2);
     printf(" Densidade Populacional: %.2f\n", DensPop2);
     printf(" PIB per capta: %.2f\n", PIBpc2);
     printf(" Número de pontos turísticos: %d\n", atracoes2);
